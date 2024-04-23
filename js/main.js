4de22b0e5437071dc023585a67c11e371e5366fa
Yachmenev-Sergey <s_yachmenev@list.ru>
2024-04-23 22:41:03 +0500

    JS: переключения меню

--- js/main.js
+++ js/main.js
@@ -2,19 +2,49 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenu = document.querySelector(".m-menu-toogle");
 const mobileMenu = document.querySelector(".mobile");
-const body = document.querySelector("body");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar--light");
+  logo.href.baseVal = "./img/sprite.svg#logo";
+};
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar--light");
+  logo.href.baseVal = "./img/sprite.svg#logo-light";
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar--light");
-    logo.href.baseVal = "./img/sprite.svg#logo";
-  } else {
-    navbar.classList.remove("navbar--light");
-    logo.href.baseVal = "./img/sprite.svg#logo-light";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
+
+const openMenu = (event) => {
+  //функция открывания меню
+  mobileMenu.classList.add("is-open");
+  mMenu.classList.add("menu-close");
+  document.body.style.overflow = "hidden";
+  lightModeOn();
+};
+//функция закрывания меню
+const closeMenu = (event) => {
+  mobileMenu.classList.remove("is-open");
+  mMenu.classList.remove("menu-close");
+  document.body.style.overflow = "";
+  lightModeOn();
+};
 
 mMenu.addEventListener("click", (event) => {
   event.preventDefault();
-  mobileMenu.classList.toggle("is-open");
-  body.classList.toggle("open");
+  if (mobileMenu.classList.contains("is-open")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
 });
+
+const madalCloseScroll = (event) => {
+  if (mobileMenu.classList.contains(".mobile")) {
+    lightModeOff();
+  } else {
+    lightModeOn();
+  }
+};
